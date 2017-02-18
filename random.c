82e3e9883d8c159519ab754984c6a95d34a62042
Jim <evvlasov@mephi.ru>
2017-02-18 12:03:32 +0300

    first release

---
+++ random.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (int argc, char *argv[]){
+int n[9];
+srand(atoi(argv[1]));
+	for (int i = 0; i < 10; i++){
+		n[i] = rand () % 10 + 1;
+		printf ("%i\n", n[i]);
+	}	
+}
