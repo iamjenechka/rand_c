dc170f8f545525baa3b022987f64a7bfe4a5c2a6
Jim <evvlasov@mephi.ru>
2017-02-19 22:22:20 +0300

    third release

--- random.c
+++ random.c
@@ -3,8 +3,5 @@
 int main (int argc, char *argv[]){
 int n[9];
 srand(atoi(argv[1]));
-	for (int i = 0; i < 10; i++){
-		n[i] = rand () % 10 + 1;
-		printf ("%i\n", n[i]);
-	}	
+	for (int i = 0; i < 10; i++){ n[i] = rand () % 10 + 1; printf ("%i\n", n[i]); }	
 }
