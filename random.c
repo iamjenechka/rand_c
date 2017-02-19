1f8ee897a06b925ee60e3b1076a1ef8fecd50748
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2017-02-19 12:56:23 +0300

    "astyle"-ed

--- random.c
+++ random.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main (int argc, char *argv[]){
-int n[9];
-srand(atoi(argv[1]));
-	for (int i = 0; i < 10; i++){
+int main ( int argc, char *argv[] )
+{
+	int n[9];
+	srand ( atoi ( argv[1] ) );
+
+	for ( int i = 0; i < 10; i++ ) {
 		n[i] = rand () % 10 + 1;
-		printf ("%i\n", n[i]);
-	}	
+		printf ( "%i\n", n[i] );
+	}
 }
